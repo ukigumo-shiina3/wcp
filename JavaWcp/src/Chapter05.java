8570d8ae281ca7fe54a63c3b1d818d5517a6d9c1
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-10 14:21:32 +0900

    [Finish]chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+	Long l = 9223372036854775807L;
+	System.out.println(l);
+	Double d =1234567.89d;
+	System.out.println(d);
+	Boolean b = true;
+	System.out.println(b);
+	
+    Integer i = 100;
+    System.out.println(i);
+    Float f = 1.4142f;
+    System.out.println(f);
+    Character c = 'う';
+    System.out.println(c);
+
+    int[] array = { 0, 10, 20, 30, 40 };
+    System.out.println(array[3]);
+  }
+}
