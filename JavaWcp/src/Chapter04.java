d1941df7ebd3e76e155ab20b1ff4c6a7c16121ea
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-10 12:38:49 +0900

    [Finish]chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		 final int  LUCKY_NUMBER = 2;
+		 String str  = "Daisuke";
+		 System.out.println(str);
+		 str = "Osaka";
+		 System.out.println(str);
+	     System.out.println(LUCKY_NUMBER);
+	}
+}
