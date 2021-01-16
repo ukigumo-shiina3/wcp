8d95f56c7cd034b592c04a2e1116b5388572b2a8
Daisuke Tanaka <daisukeyao@outlook.jp>
2021-01-16 14:25:04 +0900

    [Finish]chapter12

---
+++ JavaWcp/src/capsule/human/HumanCapsule.java
@@ -0,0 +1,28 @@
+package capsule.human;
+
+public class HumanCapsule {
+	// privateのアクセス修飾子を設定して外部から隠蔽
+    private String name;
+    private int age;
+
+    public HumanCapsule(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // nameのgetterメソッド(フィールドの値を取得)
+    public String getName() {
+        return this.name;
+    }
+
+    // nameのsetterメソッド(メソッドや値を変更)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // ageのgetterメソッド
+    // ageは外部から変更禁止なので、setterメソッドは作成しない
+    public int getAge() {
+        return this.age;
+    }
+}
